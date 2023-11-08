505f97b033dc8ab7b10e9708258c70b235f3b126
Ramiro <ramirosales52@gmail.com>
2023-11-08 12:14:09 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const isDev = require('electron-is-dev')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  if (isDev) {
+    win.loadURL('http://localhost:3000')
+  } else {
+    win.loadFile("src/build/index.html")
+  }
+
+  
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
